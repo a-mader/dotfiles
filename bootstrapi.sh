d60c6a57a6edacb58b2a299ad3d33bba217d6cb5
Amadeus Mader <amader@etkn.de>
2024-06-20 09:24:17 +0200

    feat: :sparkles: added zshrc and zinit

--- bootstrapi.sh
+++ bootstrapi.sh
@@ -29,6 +29,23 @@ apt-get install -y nala
 # Install the required packages
 nala install -y build-essential wget curl git zip unzip vim net-tools iputils-ping dnsutils netcat-traditional gpg passwd fonts-firacode tmux ripgrep sed unzip jq tree btop
 
+# Cloning the .zshrc file to the home directory
+
+wget -O ~/.zshrc https://raw.githubusercontent.com/a-mader/dotfiles/main/zshrc
+
+# Installing zinit
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+# Source the .zshrc file
+source ~/.zshrc
+
+# Zinit self update
+
+zinit self-update
+
+
+# Change the shell to zsh
+usermod -s /usr/bin/zsh $USER
 
 exit 0
 
---
+++ zshrc
@@ -0,0 +1,8 @@
+alias dps='docker ps'
+alias dpa='docker ps -a'
+alias dlogs='docker logs'
+alias dc='docker compose'
+alias dcud='docker compose up -d'
+alias dcps='docker compose ps'
+alias dcd='docker compose down'
+
